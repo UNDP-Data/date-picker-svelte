203d4a3ceedb1d54c0e4521c21b08e8182706b9f
Jin Igarashi <igarashi.jin@water-gis.com>
2024-10-28 17:13:34 +0900

    format

--- src/lib/DatePicker.svelte
+++ src/lib/DatePicker.svelte
@@ -170,7 +170,7 @@
 		return enabledDates?.length > 0
 			? enabledDates.filter((day) => {
 					return day?.getFullYear() === year
-			  }).length === 0
+				}).length === 0
 			: false
 	}
 
@@ -178,7 +178,7 @@
 		return enabledDates?.length > 0
 			? enabledDates.filter((day) => {
 					return day?.getFullYear() === date.getFullYear() && day?.getMonth() === date.getMonth()
-			  }).length === 0
+				}).length === 0
 			: false
 	}
 
